f9e4710d6557b8ec6ec38ea9fffcae6dfcc07b73
Kimio Nishino <kimionishino1@gmail.com>
2022-07-20 01:10:37 -0400

    Created carousel class
    Finally finished after one month away :+1:

--- src/script/carousel.js
+++ src/script/carousel.js
@@ -2,7 +2,6 @@ class Carousel {
     config = {};
 
     container = null;
-
     leftContainer = null;
     imageContainer = null;
     rightContainer = null;
@@ -22,59 +21,86 @@ class Carousel {
         this.container = config.container;
 
         this.setContainerStyle();
-        this.createSelectors();
+        this.createCarets();
         this.preloadImages();
-        // this.renderImg();
+        this.renderImg();
 
         console.log("finishing carousel");
     }
 
     renderImg() {
+        this.imageContainer.innerHTML = "";
         this.imageContainer.appendChild(this.imgEls[this.currentImgIndex]);
     }
 
     setContainerStyle() {
-        this.container.style.border = "1px solid black";
-        this.container.style.maxWidth = "300px";
-        this.container.style.maxHeight = "300px";
+        this.container.style.display = "flex";
+        this.container.style.alignItems = "center";
+        this.container.style.border = "1px solid #cccccc";
+        this.container.style.width = "fit-content";
+        // this.container.style.height = "300px";
     }
 
     preloadImages() {
-        this.config.imgs.forEach(img => {
-            console.log("preloading image:", img);
+        this.config.imgs.forEach((img, i) => {
+            // console.log("preloading image:", img);
             const el = document.createElement("img");
-            el.src = this.config.imgs[this.currentImgIndex];
+
+            el.src = this.config.imgs[i];
             el.style.maxWidth = "300px";
             el.style.maxHeight = "300px";
             el.style.borderRadius = "50%";
+            el.style.border = "1px solid black";
+            el.style.margin = "1px";
 
             this.imgEls.push(el);
         });
     }
 
-    createSelectors() {
+    createCarets() {
         this.leftContainer = document.createElement("div");
         this.imageContainer = document.createElement("div");
         this.rightContainer = document.createElement("div");
 
         const leftCaret = document.createElement("img");
         leftCaret.src = "./assets/caret.svg";
-        leftCaret.style.transform = "rotate(90deg)";
+        leftCaret.style.transform = "rotate(270deg)";
+        leftCaret.style.width = "32px";
 
-        console.log(leftCaret);
-
-        this.leftContainer.style.border = "1px solid green";
         this.leftContainer.appendChild(leftCaret);
+        this.leftContainer.style.cursor = "pointer";
+        this.leftContainer.onclick = () => {
+            this.currentImgIndex--;
+            if (this.currentImgIndex < 0) {
+                this.currentImgIndex = this.imgEls.length - 1;
+            }
+            this.renderImg();
+            // console.log(this.currentImgIndex);
+        }
+
 
         const rightCaret = document.createElement("img");
         rightCaret.src = "./assets/caret.svg";
-        rightCaret.style.transform = "rotate(270deg)";
+        rightCaret.style.transform = "rotate(90deg)";
+        rightCaret.style.width = "32px";
 
-        this.rightContainer.style.border = "1px solid blue";
         this.rightContainer.appendChild(rightCaret);
+        this.rightContainer.style.cursor = "pointer";
+        this.rightContainer.onclick = () => {
+            this.currentImgIndex++;
+            if (this.currentImgIndex >= this.imgEls.length) {
+                this.currentImgIndex = 0;
+            }
+            this.renderImg();
+            // console.log(this.currentImgIndex);
+        }
 
-        this.imageContainer.style.border = "1px solid red";
 
+        // this.leftContainer.style.border = "1px solid green";
+        // this.rightContainer.style.border = "1px solid blue";
+        // this.imageContainer.style.border = "1px solid red";
+
+        this.imageContainer.style.margin = "3px";
 
         this.container.appendChild(this.leftContainer);
         this.container.appendChild(this.imageContainer);
